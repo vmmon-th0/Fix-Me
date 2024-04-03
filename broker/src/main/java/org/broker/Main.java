19935d70f15c4aad0801e0a0efa265bafbc1b10c
vmmon-th0 <ammonschiffer@outlook.com>
2024-04-03 23:55:40 +0200

    feat: Establish a TCP connection, between broker and router

--- broker/src/main/java/org/broker/Main.java
+++ broker/src/main/java/org/broker/Main.java
@@ -1,7 +1,52 @@
 package org.broker;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void cleanSocketsIO() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void joinMarket(String ip, int port) {
+        System.out.printf("Enter join market process");
+        try {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            out.println("hello router");
+
+            String resp = in.readLine();
+            System.out.println("Response to broker: " + resp);
+
+            cleanSocketsIO();
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.printf("Hello from broker!");
+        Main mainObject = new Main();
+        mainObject.joinMarket("127.0.0.1", 5000);
     }
 }
--- router/src/main/java/org/router/Main.java
+++ router/src/main/java/org/router/Main.java
@@ -1,10 +1,66 @@
 package org.router;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private ServerSocket serverSocketBroker;
+    private ServerSocket serverSocketMarket;
+    private Socket clientSocketMarket;
+    private Socket clientSocketBroker;
+    private PrintWriter bOut, mOut;
+    private BufferedReader bIn, mIn;
+
+    public void cleanSocketsIO() {
+        try {
+            bIn.close();
+            mIn.close();
+            bOut.close();
+            mOut.close();
+            clientSocketMarket.close();
+            clientSocketBroker.close();
+            serverSocketBroker.close();
+            serverSocketMarket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void marketConnectivityProvider() {
+        try {
+            serverSocketBroker = new ServerSocket(5000);
+//            serverSocketMarket = new ServerSocket(5001);
+
+            clientSocketBroker = serverSocketBroker.accept();
+//            clientSocketMarket = serverSocketMarket.accept();
+
+            bOut = new PrintWriter(clientSocketBroker.getOutputStream(), true);
+//            mOut = new PrintWriter(clientSocketMarket.getOutputStream(), true);
+
+            bIn = new BufferedReader(new InputStreamReader(clientSocketBroker.getInputStream()));
+//            mIn = new BufferedReader(new InputStreamReader(clientSocketMarket.getInputStream()));
+
+            String greeting = bIn.readLine();
+            if ("hello router".equals(greeting)) {
+                bOut.println("hello broker");
+            }
+            else {
+                bOut.println("unrecognised greeting");
+            }
+
+            cleanSocketsIO();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello from router!");
+        System.out.println("Hello from router, we dispatch messages");
+        Main mainObject = new Main();
+        mainObject.marketConnectivityProvider();
     }
 }
